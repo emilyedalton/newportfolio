79d80ad5c75d0d9844640ee47c30e7cf790d06e0
Emily Dalton <emilyedalton@gmail.com>
2019-06-11 13:19:36 -0400

    adding centered intro on mainpage

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Button, Container, Grid, Header } from 'semantic-ui-react'
 import { Parallax, Background } from 'react-parallax';
 
@@ -8,43 +8,88 @@ const insideStyles = {
     top: "50%",
     left: "50%",
     transform: "translate(-50%,-50%)"
-  };
+};
 
 class Main extends Component {
 
-    render (){
+    render() {
         return (
-          <div>
+            <div>
 
-    <Parallax
-            bgImage={require('../../assets/images/Bridge.jpg')}
-            bgImageAlt="myBackground"
-            strength={200}
-        >
-        <Container>
-            <Grid>
-        <Grid.Column style={{border: "solid 2px red"}}width={8} textAlign="center">
+                <Parallax
+                    bgImage={require('../../assets/images/Bridge.jpg')}
+                    bgImageAlt="myBackground"
+                    strength={200}
+                >
 
-            <div style={{ height: '100vh' }} />
+                    <Container style={{ height: '100vh' }} >
 
-                   </Grid.Column>
-
-                     <Grid.Column style = {{border: "solid 2px blue"}} width={8} textAlign="center">
-                     <Header as='h1' content="Emily Dalton | Web Developer" 
-         style={{
-             fontSize: '2em',
-             color:'white'}}/>
-            <Button style ={{}} inverted>View Portfolio</Button>
-      </Grid.Column>
-     
-      </Grid>
-      </Container>
-    </Parallax>
-
- 
+                        <Grid  columns={3} stackable={1}>
 
 
-</div>
-    )
-}}
+                            <Grid.Column style={{ border: "solid 2px red" }}width={4} textAlign="center"/>
+
+                            <Grid.Column style={{ border: "solid 2px red" }} width={8} textAlign="center">
+
+
+
+                                    <div style={{height:"4em"}}/>
+
+                            </Grid.Column>
+                           
+                            <Grid.Column style={{ border: "solid 2px red" }} width={4} textAlign="center"/>
+<Grid.Row style={{border: "2px solid green"}}/>
+<Grid.Column style={{ border: "solid 2px red" }}width={4} textAlign="center"/>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={8} textAlign="center">
+
+
+
+        <div style={{height:"4em"}}/>
+
+</Grid.Column>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={4} textAlign="center"/>
+<Grid.Row style={{border: "2px solid green"}}/>
+
+<Grid.Column style={{ border: "solid 2px red" }}width={4} textAlign="center"/>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={8} textAlign="center">
+
+
+
+    <Header as='h1' content="Emily Dalton | Web Developer"
+        style={{
+            fontSize: '2em',
+            color: 'white',
+        }} />
+    <Button style={{}} inverted>View Portfolio</Button>
+</Grid.Column>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={4} textAlign="center"/>
+<Grid.Row style={{border: "2px solid green"}}/>
+
+                            <Grid.Column style={{ border: "solid 2px red" }}width={4} textAlign="center"/>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={8} textAlign="center">
+
+
+
+    <div style={{height:"4em"}}
+        />
+</Grid.Column>
+
+<Grid.Column style={{ border: "solid 2px red" }} width={4} textAlign="center"/>
+<Grid.Row style={{border: "2px solid green"}}/>
+                        </Grid>
+                    </Container>
+                </Parallax>
+
+
+
+
+            </div>
+        )
+    }
+}
 export default Main; 
