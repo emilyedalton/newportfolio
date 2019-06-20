da4484d425f27f9bdabb311b683e3d7699275a29
Emily Dalton <emilyedalton@gmail.com>
2019-06-20 10:51:26 -0500

    beginings of footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { Container, Grid} from 'semantic-ui-react'
+import { Parallax } from 'react-parallax';
+
+class Footer extends Component {
+
+    render (){
+        return (
+            
+            <div style={{ height: '150px', background: "rgba(0, 0, 0, 0.8)" }} >
+             <Parallax>
+                 
+           <p style={{color: "white", margin: "auto"}}> © Emily Dalton</p>
+           
+                 </Parallax>
+                 </div>
+        )}}
+
+ export default Footer
