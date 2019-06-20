c0a978a6ef88d0e97993a2534a777b328f17cdc1
Emily Dalton <emilyedalton@gmail.com>
2019-06-20 09:27:29 -0500

    built out card content

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -77,9 +77,28 @@ class Portfolio extends Component {
                       as='a'
                       href='http://google.com'
                       target='_blank' />
+                       <Card.Content>
+                      <Card.Header>Food Finder</Card.Header>
+                      <Card.Meta>Edamam API • JQuery • CSS FlexBox </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                   </Card>
                   <Card>
-                    <Image src={require('../../assets/images/Train_Shedule.jpg')} />
+                    <Image src={require('../../assets/images/Train_Shedule.jpg')}  as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                    <Card.Content>
+                      <Card.Header>Train Scheduler</Card.Header>
+                      <Card.Meta>Bootstrap • JQuery • Firebase • Moment.js </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
+
                   </Card>
 
                   <Card>
@@ -87,6 +106,14 @@ class Portfolio extends Component {
                       as='a'
                       href='http://google.com'
                       target='_blank' />
+                       <Card.Content>
+                      <Card.Header>Burgers!</Card.Header>
+                      <Card.Meta> Mongoose • HandlebarsJS • CSS FlexBox </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                   </Card>
                 </Card.Group>
             
@@ -94,26 +121,86 @@ class Portfolio extends Component {
               <Card.Group itemsPerRow={3} stackable columns={1}>
                   
                    <Card>
-                    <Image src={require('../../assets/images/friend_findr.jpg')} />
+                    <Image src={require('../../assets/images/friend_findr.jpg')}  as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                    <Card.Content>
+                      <Card.Header>Man's Best Friend Finder</Card.Header>
+                      <Card.Meta>Express • Bootstrap </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                   </Card>
                   <Card>
-                    <Image src={require('../../assets/images/LetterGuessGame.jpg')} />
+                    <Image src={require('../../assets/images/LetterGuessGame.jpg')}  as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                    <Card.Content>
+                      <Card.Header>Letter Guess Game</Card.Header>
+                      <Card.Meta>JavaScript • CSS FlexBox  </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                   </Card>
                   <Card>
-                    <Image src={require('../../assets/images/TriviaGame.jpg')} />
+                    <Image src={require('../../assets/images/TriviaGame.jpg')}  as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                    <Card.Content>
+                      <Card.Header>Chicago Style Trivia</Card.Header>
+                      <Card.Meta>JavaScript • CSS FlexBox </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                   </Card>
                 </Card.Group>
                
                 <Card.Group itemsPerRow={3} stackable columns={1}>
       
                   <Card>
-                  <Image src={require('../../assets/images/Obect_Collector.jpg')} />
+                  <Image src={require('../../assets/images/Obect_Collector.jpg')}  as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                  <Card.Content>
+                      <Card.Header>Object Collector</Card.Header>
+                      <Card.Meta>JavaScript • CSS FlexBox </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                 </Card>
                 <Card>
-                  <Image src={require('../../assets/images/liri.jpg')} />
+                  <Image src={require('../../assets/images/liri.jpg')} as='a'
+                      href='http://google.com'
+                      target='_blank' />
+                  <Card.Content>
+                      <Card.Header>Liri Bot</Card.Header>
+                      <Card.Meta>Bands in Town API • Spotify API • OMDB API • NodeJs </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                 </Card>
                 <Card>
-                <Image src={require('../../assets/images/Bamazon.jpg')} />
+                <Image src={require('../../assets/images/Bamazon.jpg')}  as='a'
+                      href='https://github.com/emilyedalton/bamazon/blob/master/README.md'
+                      target='_blank' />
+                <Card.Content>
+                <Card.Header>Bamazon</Card.Header>
+                      <Card.Meta>MySQL • NodeJs </Card.Meta>
+                      <Card.Description>
+                      </Card.Description>
+                      <Button>Github</Button>
+                      <Button>Link</Button>
+                    </Card.Content>
                 </Card>
               </Card.Group>
            
